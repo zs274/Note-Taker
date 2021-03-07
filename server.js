990f83b4df07c0bd813d7c7bdfd6a19260cc0317
Zarin Salim <zarin_96@hotmail.co.uk>
2021-03-07 18:17:18 +0000

    issues connecting files

--- server.js
+++ server.js
@@ -2,12 +2,10 @@ const express = require('express');
 const path = require('path');
 const fs = require('fs');
 const uniqid = require('uniqid');
-const { debugPort } = require('process');
-
 const app = express();
 const PORT = process.env.PORT || 3000;
 
-// making path of public folder into a const for easier linking of html files
+// making path of public folder into a const for easier linking of files
 const main = path.join(__dirname, 'public');
 
 app.use(express.static('public'));
@@ -18,27 +16,27 @@ app.use(express.json);
 app.get('/', (req, res) => res.sendFile(path.join(main, 'index.html')));
 app.get('/notes', (req, res) => res.sendFile(path.join(main, 'notes.html')));
 
-app.get('/api/notes', function (req, res) {
+app.get('Develop/public/assets/js/index.js', (req, res) => res.sendFile(path.join('Develop/public/assets/js/index.js')));
+app.get('Develop/public/assets/css/styles.css', (req, res) => res.sendFile(path.join('Develop/public/assets/css/styles.css')));
+
+
+app.get('/api/notes', (req, res) => {
     res.sendFile(path.join(__dirname, './db/db.json'));
 });
 
-app.get('/api/notes/:id', function (req, res) {
-    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
-    res.json(savedNotes[Number(req.params.id)]);
+app.get('/api/notes/:id', (req, res) => {
+    let saved = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    res.json(saved[Number(req.params.id)]);
 });
 
 app.post('/api/notes', (req, res) => {
-    let savedNotes = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
+    let saved = JSON.parse(fs.readFileSync('./db/db.json', 'utf8'));
     let newNote = req.body;
 
     newNote.id = uniqid();
-    savedNotes.push(newNote);
-
-
-})
-
-
-
+    saved.push(newNote);
+    fs.writeFileSync('./db/db.json', JSON.stringify(saved));
+    res.json(saved);
+});
 
 app.listen(PORT, () => console.log('listening on port ${PORT}'));
-
